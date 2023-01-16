9258299de78eded0ef066cec1bad86b42a5c2bee
Fleksis <markuss0303@gmail.com>
2023-01-16 22:22:30 +0200

    feat: deleted db seeder create method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,10 +39,5 @@ class DatabaseSeeder extends Seeder {
         ]);
 
         LifeSchool::factory()->times(100)->create();
-
-//        ChatRoom::create([
-//            'user1_id' => 1002,
-//            'user2_id' => 1003,
-//        ]);
     }
 }
